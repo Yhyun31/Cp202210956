29021b83cf6c21163ff2d653e152efe941673714
Yhyun31 <112770356+Yhyun31@users.noreply.github.com>
2022-09-29 18:26:09 +0900

    Add files via upload

---
+++ hw4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void)
+{
+	int num,i;
+	int count = 0;
+
+	printf("Please enter a number:");
+	scanf("%d", &num);
+
+	for (i = 2;i <= num;i++)
+	{
+		if (num % i == 0)
+			count++;
+	}
+	if (count>1 || num==1)
+		printf("It is not a prime number");
+	else
+		printf("It is a prime number");
+	return 0;
+}
